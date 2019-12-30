88e8c0b8d42cff509a9f4c9728d3754d98931a52
Imran Ali <ali@copperstones.net>
2019-12-30 16:52:30 +0500

    passport providers configur

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -4,9 +4,20 @@ namespace App\Providers;
 
 use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
+use Laravel\Passport\Passport;
+use Illuminate\Support\Facades\Gate; 
 
 class AppServiceProvider extends ServiceProvider
 {
+    /** 
+     * The policy mappings for the application. 
+     * 
+     * @var array 
+     */ 
+    protected $policies = [ 
+        'App\Model' => 'App\Policies\ModelPolicy', 
+    ];
+
     /**
      * Register any application services.
      *
@@ -26,5 +37,8 @@ class AppServiceProvider extends ServiceProvider
     {
         // Default string length if not defined in migration
         Schema::defaultStringLength(191);
+
+        $this->registerPolicies(); 
+        Passport::routes();
     }
 }
