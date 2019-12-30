67f6d690fc1730faeac3036423d89471eaecb457
Imran Ali <ali@copperstones.net>
2019-12-30 17:25:27 +0500

    request changes

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -19,11 +19,11 @@ class UserController extends Controller
     		return response()->json(['error'=>$validator->errors()], 401);
     	}
 
-    	$input = $request->all(); 
+    	/*$input = $request->all(); 
     	$input['password'] = bcrypt($input['password']); 
     	$user = User::create($input); 
-    	$success['token'] =  $user->createToken('MyApp')-> accessToken; 
+    	$success['token'] =  $user->createToken('MyApp')->accessToken; 
     	$success['name'] =  $user->name;
-    	return response()->json(['success'=>$success], $this-> successStatus); 
+    	return response()->json(['success'=>$success], $this->successStatus); */
     }
 }
