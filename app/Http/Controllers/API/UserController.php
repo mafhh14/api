3b861d3e52d6f3e60b6574ccc2cf6ddde35b42a2
Imran Ali <ali@copperstones.net>
2019-12-30 17:32:56 +0500

    basic user validation

--- app/Http/Controllers/API/UserController.php
+++ app/Http/Controllers/API/UserController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\API;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Http\Requests\UserAPI;
+use Validator;
 
 class UserController extends Controller
 {
@@ -13,17 +13,24 @@ class UserController extends Controller
      * 
      * @return \Illuminate\Http\Response 
      */ 
-    public function register(UserAPI $request) 
+    public function register(Request $request) 
     {
-    	if (!$request->validated()) { 
-    		return response()->json(['error'=>$validator->errors()], 401);
-    	}
+        $validator = Validator::make($request->all(), [ 
+            'name' => 'required', 
+            'email' => 'required|email', 
+            'password' => 'required', 
+            'c_password' => 'required|same:password', 
+        ]);
 
-    	/*$input = $request->all(); 
-    	$input['password'] = bcrypt($input['password']); 
-    	$user = User::create($input); 
-    	$success['token'] =  $user->createToken('MyApp')->accessToken; 
-    	$success['name'] =  $user->name;
-    	return response()->json(['success'=>$success], $this->successStatus); */
+        if ($validator->fails()) { 
+            return response()->json(['error'=>$validator->errors()], 401);
+        }
+
+				$input = $request->all(); 
+        $input['password'] = bcrypt($input['password']); 
+        $user = User::create($input); 
+        $success['token'] =  $user->createToken('MyApp')-> accessToken; 
+        $success['name'] =  $user->name;
+				return response()->json(['success'=>$success], $this-> successStatus); 
     }
 }
